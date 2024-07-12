b11dbd786cccdf3e053529c0229c53d36e6ed7fb
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-07-12 16:01:43 +0530

    customized the admin panel and registered the models.

--- portfolio/home/admin.py
+++ portfolio/home/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Project, ProjectImage
+
+# Admin panel customization
+
+class ProjecImageInLine(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "link",)
+    inlines = [ProjecImageInLine]
+    search_fields = ("title", "description")
+    list_filter = ("tags",)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+# registering models
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectImage)
