15356af5b2de656d8a91c3ad8ec38715302d82e2
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-07-12 15:38:42 +0530

    created models for the homepage app

--- portfolio/home/models.py
+++ portfolio/home/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+# Tag class
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self) -> str:
+        return self.title
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="projects")
+    link = models.URLField(max_length=200, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+
+# for images
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='project_images/')
+
+    def __str__(self):
+        return f"{self.project.title} Image."
