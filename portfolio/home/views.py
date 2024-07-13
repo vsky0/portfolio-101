f2cdc56851df1361f677629551ab6b40e8cbd9a2
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-07-13 14:50:55 +0530

    added views for homepage and projectpage

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Tag
+
 
 # Create your views here.
+
+def homepage(request):
+    projects = Project.objects.all()
+    tags = Tag.objects.all()
+    context = {"projects": projects, "tags": tags}
+    return render(request, "homepage.html", context)
+
+def contact(request):
+    return render(request, "contact.html")
+
+def project(request, id):
+    project = get_object_or_404(Project, pk=id)
+    context = {"project":project}
+    return render(request, "project.html", context)
